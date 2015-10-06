c0b9b9846d00def8d450aec2f5359aab0100028f
Nick Tsiogkas <nt95@hw.ac.uk>
2015-10-06 13:54:47 +0100

    Modify the ovrp function to accept a cost matrix. Added debug option to print messages. Did stylistic changes to be compliant with Python syntax.

--- ovrp_solver.py
+++ ovrp_solver.py
@@ -7,6 +7,7 @@ import numpy as np
 from gurobipy import *
 
 HAS_GUROBI = True
+DEBUG = False
 
 # Euclidean distance between two points
 
@@ -15,24 +16,24 @@ def distance(points, i, j):
     return np.linalg.norm(points[i, :] - points[j, :])
 
 
-def ovrp_solver(cities, start=None, finish=None):
+def ovrp_solver(cost, start=None, finish=None):
 
     """
     Open vehicle routing problem solver for a single vehicle using the Gurobi MILP optimiser.
-    :param cities: List of points to optimise the route. Numpy NxM array, where M is in [2, 3] (dimension of the points)
+    :param cost: Cost matrix for traveling from point to point.
     :param start: Optional starting point for the tour. If none is provided the first point of the array is chosen
     :param finish: Optional ending point of the tour. If none is provided the last point of the array is chosen
     :return: Returns the route the cost and the model.
     """
 
+    # Number of points
+    n = cost.shape[0]
+
     # Check for default values
     if start is None:
         start = 0
     if finish is None:
-        finish = len(cities)-1
-
-    # Number of points
-    n = len(cities)
+        finish = n - 1
 
     m = Model()
 
@@ -40,13 +41,12 @@ def ovrp_solver(cities, start=None, finish=None):
     vars = {}
     for i in range(n):
         for j in range(n):
-            if i == j:
-                dist = 0
-                vars[i,j] = m.addVar(obj=dist, vtype=GRB.BINARY, name='e'+str(i)+'_'+str(j), ub=0)
-            else:
-                dist = distance(cities, i, j)
-                vars[i,j] = m.addVar(obj=dist, vtype=GRB.BINARY, name='e'+str(i)+'_'+str(j))
+            vars[i, j] = m.addVar(obj=cost[i, j], vtype=GRB.BINARY, name='e'+str(i)+'_'+str(j))
 
+    m.update()
+
+    for i in range(n):
+        vars[i, i].ub = 0
     m.update()
 
     uVars = {}
@@ -55,30 +55,30 @@ def ovrp_solver(cities, start=None, finish=None):
     m.update()
 
     # None exits the finish point
-    m.addConstr(quicksum(vars[finish,j] for j in range(n)) == 0)
+    m.addConstr(quicksum(vars[finish, j] for j in range(n)) == 0)
     m.update()
 
     # From all other points someone exits
     for i in range(n):
         if i != finish:
-            m.addConstr(quicksum(vars[i,j] for j in range(n)) == 1)
+            m.addConstr(quicksum(vars[i, j] for j in range(n)) == 1)
     m.update()
 
     # None enters the starting point
-    m.addConstr(quicksum(vars[j,start] for j in range(n)) == 0)
+    m.addConstr(quicksum(vars[j, start] for j in range(n)) == 0)
     m.update()
 
     # To all other points someone enters
     for i in range(n):
         if i != start:
-            m.addConstr(quicksum(vars[j,i] for j in range(n)) == 1)
+            m.addConstr(quicksum(vars[j, i] for j in range(n)) == 1)
     m.update()
 
     # Sub-tour elimination constraint
     for i in range(n):
         for j in range(n):
-            if (i != j):
-                m.addConstr(uVars[i]-uVars[j]+n*vars[i,j] <= n-1)
+            if i != j:
+                m.addConstr(uVars[i] - uVars[j] + n * vars[i, j] <= n-1)
     m.update()
 
     m._vars = vars
@@ -88,16 +88,17 @@ def ovrp_solver(cities, start=None, finish=None):
     try:
         solution = m.getAttr('X', vars)
         u = m.getAttr('X', uVars)
-        selected = [(i,j) for i in range(n) for j in range(n) if solution[i,j] > 0.5]
+        selected = [(i, j) for i in range(n) for j in range(n) if solution[i, j] > 0.5]
 
-        mat = np.zeros((n,n))
+        if DEBUG:
+            mat = np.zeros((n, n))
 
-        for k,v in solution.iteritems():
-            mat[k[0],k[1]] = v
+            for k,v in solution.iteritems():
+                mat[k[0], k[1]] = v
 
-        print(mat)
-        print(selected)
-        print(u)
+            print(mat)
+            print(selected)
+            print(u)
 
         route = np.zeros(n, dtype=np.int)
 
@@ -154,10 +155,11 @@ def main():
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(ips[:, 1], ips[:, 0], ips[:,2], 'o', label='inspection points')
         ax.plot(ips_route[:, 1], ips_route[:, 0], ips_route[:, 2], 'r-', alpha=0.3)
+        return fig, ax
 
     # generate random problem
-    n = 20
-    points = np.random.randint(-50, 50, (n, 3))
+    n = 12
+    points = np.random.randint(-50, 50, (n, 2))
     cities = ['c_{}'.format(k) for k in xrange(n)]
 
     # standard cost
@@ -170,7 +172,7 @@ def main():
     if HAS_GUROBI:
         # solve using the Gurobi solver
         st = time.time()
-        tsp_route, total_cost, model = ovrp_solver(points, 5, 6)
+        tsp_route, total_cost, model = ovrp_solver(distances, 1, 2)
         dt = time.time() - st
 
         print('Gurobi Solver')
@@ -180,10 +182,12 @@ def main():
 
 
 
-        fig, ax = __plot_problem(points, tsp_route, total_cost)
+
 
         if points.shape[1] == 3:
-            __plot_problem3d(points, tsp_route, total_cost)
+            fig, ax = __plot_problem3d(points, tsp_route, total_cost)
+        else:
+            fig, ax = __plot_problem(points, tsp_route, total_cost)
 
         plt.show()
 
